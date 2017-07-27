be584a013685e6ac7dd63c12cda8fd8e600e10c0
Collin OConnell <ococncol@gmail.com>
2017-07-26 21:23:18 -0400

    impletemted db helper and setup schema for the db table and creating it

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -2,6 +2,9 @@ package com.bignerdranch.android.criminalintent;
 
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.bignerdranch.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +14,13 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
         mCrimes = new ArrayList<>();
     }
 
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.criminalintent.database;
+
+/**
+ * Created by Shortnd on 7/26/17.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
